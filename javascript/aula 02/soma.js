447916bc09de1d59ce60ec4801125bda01cc4e65
Luis Monezi <luis.monezi@ags-it.com>
2022-09-27 21:57:19 +0200

    Aula 01 - Introducao ao Javascript

---
+++ javascript/aula 02/soma.js
@@ -0,0 +1,11 @@
+let soma1 = somar(5, 13);
+let soma2 = somar(323, 3);
+let soma3 = somar(-5, -18);
+
+console.log(soma1);
+console.log(soma2);
+console.log(soma3);
+
+function somar(numero1, numero2) {
+	return numero1 + numero2;
+}
