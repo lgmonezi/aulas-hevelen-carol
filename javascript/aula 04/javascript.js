db5c751d337be40a082727fdc0092b43fe3f09c3
Luis Monezi <luis.monezi@ags-it.com>
2022-09-28 19:19:47 +0200

    Aula 04 - inicio

---
+++ javascript/aula 04/javascript.js
@@ -0,0 +1,12 @@
+// Tipos de operadores:
+// 	Operadores Logicos (comparacao):
+//			> maior que
+//			< menor que
+//			>= maior ou igual a
+//			<= menor ou igual a
+//			== igual
+//			!= diferente
+
+//		Operadores Logicos (execucao):
+//			if(boolean)			(verifica condição)
+//			while(boolean) 	(looping)
