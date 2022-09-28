b47b67197c215e3ad7ce2a2ff6302af315a59f37
Luis Monezi <luis.monezi@ags-it.com>
2022-09-28 19:28:49 +0200

    Comentarios tipos e operadores

--- javascript/aula 04/javascript.js
+++ javascript/aula 04/javascript.js
@@ -1,12 +1,14 @@
-// Tipos de operadores:
-// 	Operadores Logicos (comparacao):
-//			> maior que
-//			< menor que
-//			>= maior ou igual a
-//			<= menor ou igual a
-//			== igual
-//			!= diferente
+/*
+Tipos de operadores:   									Tipos de Variaveis:   
+	Operadores Logicos (comparacao):   				
+		> maior que   										Tipo string (texto)
+		< menor que   										Tipo numero
+		>= maior ou igual a   							Tipo numero
+		<= menor ou igual a   							Tipo numero
+		== igual   											Tipo booleado (true ou false)
+		!= diferente   									Tipo booleado (true ou false)
+	Operadores Logicos (execucao):
+		if(boolean)			(verifica condição)
+		while(boolean) 	(looping)
+*/
 
-//		Operadores Logicos (execucao):
-//			if(boolean)			(verifica condição)
-//			while(boolean) 	(looping)
